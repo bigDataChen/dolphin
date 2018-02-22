8b66218b65c588886e699ed8bc28e4a57f427144
Edwinguo <edwinguo@Edwinguos-MacBook-Pro.local>
2018-02-21 22:34:07 -0500

    add bootstrap

--- example/model.js
+++ example/model.js
@@ -1,12 +1,12 @@
 var Joi = require('joi');
 var custom_fields = {
     name  : Joi.string().required(),
-    histroy : Joi.string().required(),
-    language : Joi.number().required(),
-    plan : Joi.string().required(),
-    deposit : Joi.number().required(),
+    //histroy : Joi.string().required(),
+    //language : Joi.number().required(),
+    //plan : Joi.string().required(),
+    //deposit : Joi.number().required(),
     asset : Joi.number().required(),
-    validation : Joi.string().required(),
+    //validation : Joi.string().required(),
     email : Joi.string().email().required(),
 };
 module.exports = custom_fields;
