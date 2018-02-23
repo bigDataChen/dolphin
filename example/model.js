934158a5b8d878b4d6baae64cb5ff380d9c87d29
Edwinguo <edwinguo@Edwinguos-MacBook-Pro.local>
2018-02-22 21:45:28 -0500

    add in the changes

--- example/model.js
+++ example/model.js
@@ -1,12 +1,12 @@
 var Joi = require('joi');
 var custom_fields = {
     name  : Joi.string().required(),
-    //histroy : Joi.string().required(),
-    //language : Joi.number().required(),
-    //plan : Joi.string().required(),
-    //deposit : Joi.number().required(),
+    history : Joi.string(),
+    language : Joi.number().required(),
+    plan : Joi.string(),
+    deposit : Joi.number().required(),
     asset : Joi.number().required(),
-    //validation : Joi.string().required(),
+    validation : Joi.string(),
     email : Joi.string().email().required(),
 };
 module.exports = custom_fields;
